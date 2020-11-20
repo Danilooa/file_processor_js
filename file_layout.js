1c9c0cd4ff570a08011b194365128f67eb362c3c
danilooa <danilooa@gmail.com>
2020-11-20 15:48:10 -0300

    tests identified layouts' types

--- file_layout.js
+++ file_layout.js
@@ -1,11 +1,49 @@
 "use strict";
 
 const file_layout = () => {
+
+    const layout = ({ code_parameter, parse_function }) => {
+        const code = code_parameter;
+        const parse = parse_function;
+        return {
+            code,
+            parse
+        };
+    }
+
+    const layouts = {};
+    layouts[`001`] = layout({
+        code_parameter: `001`,
+        parse_function: ({ row = `` }) => {
+            return row;
+        }
+    });
+    layouts[`002`] = layout({
+        code_parameter: `002`,
+        parse_function: ({ row = `` }) => {
+            return row;
+        }
+    });
+    layouts[`003`] = layout({
+        code_parameter: `003`,
+        parse_function: ({ row = `` }) => {
+            return row;
+        }
+    });
+
     const exists = () => {
         return true;
     }
+
+    const layout_from_row = ({ separator = `;`, row = `` }) => {
+        const string_fields = row.split(separator);
+        const layout_code = string_fields[0];
+        return layouts[layout_code];
+    }
+
     return {
-        exists: exists
+        exists: exists,
+        layout_from_row: layout_from_row
     };
 }
 module.exports = file_layout();
--- specs/file_layout.spec.js
+++ specs/file_layout.spec.js
@@ -1,9 +1,26 @@
-var appRoot = require(`app-root-path`);
-const { expect } = require(`chai`);
+const appRoot = require(`app-root-path`);
+const chai = require(`chai`);
+chai.use(require('chai-string'));
+const { expect } = chai;
 const file_layout = require(`${appRoot}/file_layout`);
 
 describe("file_layout", () => {
     it("should exist", () => {
         expect(file_layout.exists()).to.be.true;
     });
+    it("should identify layout 001", () => {
+        const row = `001ç1234567891234çDiegoç50000`;
+        const layout = file_layout.layout_from_row({ separator: `ç`, row: row });
+        expect(layout.code).to.equalIgnoreCase(`001`);
+    });
+    it("should identify layout 002", () => {
+        const row = `002ç2345675434544345çJosedaSilvaçRural`;
+        const layout = file_layout.layout_from_row({ separator: `ç`, row: row });
+        expect(layout.code).to.equalIgnoreCase(`002`);
+    });
+    it("should identify layout 003", () => {
+        const row = `003ç08ç[1-34-10,2-33-1.50,3-40-0.10]çRenato`;
+        const layout = file_layout.layout_from_row({ separator: `ç`, row: row });
+        expect(layout.code).to.equalIgnoreCase(`003`);
+    });    
 });
